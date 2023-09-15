a3c8af6c0af69133450d35f5ca2169b2c821f88f
Tom Hine <tomhine792@gmail.com>
2023-09-15 22:58:25 +0100

    offcanvas work

--- src/js/offcanvas.js
+++ src/js/offcanvas.js
@@ -4,6 +4,27 @@ document.addEventListener("DOMContentLoaded", () => {
     offcanvasToggles.forEach((toggle) => {
         toggle.addEventListener("click", () => {
             const target = document.querySelector(toggle.dataset.target);
+            console.log(target);
+
+            if (target.classList.contains("hidden")) {
+                const backdrop = document.createElement("div");
+                backdrop.classList.add("ts-backdrop");
+                backdrop.setAttribute("data-ts.backdrop", "");
+                backdrop.setAttribute("data-ts.target", toggle.dataset.target);
+                document.body.appendChild(backdrop);
+
+                document.body.classList.add("overflow-hidden");
+
+                target.classList.remove("hidden");
+            } else {
+                const backdrop = document.querySelector("[data-ts-backdrop]");
+
+                if (backdrop) {
+                    backdrop.classList.add("hidden");
+                }
+
+                target.classList.add("hidden");
+            }
             target.classList.toggle("hidden");
         });
     });
