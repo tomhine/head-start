86adf1828a303251aed20473142a1e6cd357beb2
Tom Hine <tomhine792@gmail.com>
2023-10-10 00:05:53 +0100

    product form

--- src/js/add-to-cart.js
+++ src/js/add-to-cart.js
@@ -1,3 +1,5 @@
+import {addToCart} from "./cart-helpers";
+
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form[data-js-cart="true"]');
 
@@ -6,7 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
 
+        const variantId = form.dataset.variantId;
 
+        const [data, error] = await addToCart([{ variantId, quantity: 1 }]);
+
+        if (error) {
+            console.error(error);
+        } else {
+            console.log(data);
+        }
 
         console.log('form submitted');
     });
