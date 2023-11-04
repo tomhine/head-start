09f94ee0669ef58c2a3f304fdad6559342449209
Tom Hine <tomhine792@gmail.com>
2023-11-04 21:03:57 +0000

    added polyfills for mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 const tailwind = require('mix-tailwindcss');
 
+require('laravel-mix-polyfill');
+
 mix.sass('src/scss/main.scss', 'assets/').tailwind('tailwind.config.js');
-mix.js('src/js/main.js', 'assets/');
+mix.js('src/js/main.js', 'assets/').polyfill();
